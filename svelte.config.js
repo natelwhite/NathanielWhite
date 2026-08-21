14acd0d22a5d2a0eca49cf9bfc0bc810027c559d
natelwhite <natelwhite01@gmail.com>
2026-08-20 22:34:43 -0400

    Deploy from branch

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 const config = {
-  kit: {
-	appDir: 'build',
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
-    }),
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+		}
 	}
-  }
 };
 export default config;
